d1f136e367b2ca6d5fcf6f807e7da80c3c8e82fa
Leonel Velez <leonelvelez314@gmail.com>
2020-10-04 13:28:34 -0500

    adding the value method

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 class Validate{
-
     static value(vale)
-    {
-        
+    {        
         let ret = true;
-
         if(vale === null)
         {
             ret = false;
+            console.warn('the value is null')
         }else{
             if(vale === undefined)
             {
                 ret = false;
+                console.warn('the value is undfined')
             }else{
                 if(vale.trim() === '')
                 {
                     ret = false;
+                    console.warn('the value is empty')
                 }
             }
         }
-
         return ret;
     }
 }
